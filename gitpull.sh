5e2dfd97a273bbdd363e8fdad57335de4a46053e
Lukas34 <lukas.langrock@outlook.de>
2019-04-09 18:04:58 +0200

    Update gitpull.sh

--- gitpull.sh
+++ gitpull.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-# This script goes into each subdirectory and executes a git pull
+#This script goes into each subdirectory and executes a git pull
+#Credit: https://stackoverflow.com/questions/7470165/how-to-go-to-each-directory-and-execute-a-command
+
+echo Updating repos...
+
+#cd ..
+#find . -type d | while read d; do
+#   ls $d/
+#   git pull
+#done
+
+cd ..
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && echo [{}]  && git pull" \;
+
+echo Update finished
