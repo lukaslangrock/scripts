1e18f397f41679abfa3557ebf8527d72e985081d
Lukas <lukas.langrock@outlook.de>
2019-07-15 11:27:18 +0200

    Create piratebox_automove.sh

---
+++ piratebox_automove.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Made to autorun on a PirateBox
+# This script will automatically sort uplaoded files
+
+cd /opt/piratebox/share/Shared                                                         
+sudo mv *.png Images/                                                                  
+sudo mv *.jpg Images/                                                                  
+sudo mv *.mp4 Videos/                                                                  
+sudo mv *.mov Videos/
