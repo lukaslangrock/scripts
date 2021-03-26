9cc527ab863e65d011d2e0865dde96ae46a4e4f7
Lukas Langrock <lukas.langrock@mailbox.org>
2021-03-26 18:01:13 +0100

    add mxf to mkv lossless convert script

---
+++ ffmpeg/mxf_to_mkv.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#Convert/Mux all MXF files losslessly to an MKV
+
+# magic that allows folders with dots to not cause issues
+IFS='
+'
+set -f
+
+#actual loop
+for i in $(find . -name '*.MXF'); do
+  echo "Generating preview: ${i}"
+  ffmpeg -i "${i}" -filter_complex "[0:a:1][0:a:0] amerge=inputs=2 [a]" -map 0:v -map "[a]" -c:v copy -c:a flac -ac 2 "${i%.*}.MKV"
+done
