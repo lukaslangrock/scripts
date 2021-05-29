bb5e10b0689e54811c6de710f4cd00ab23cd89df
Lukas Langrock <lukas.langrock@mailbox.org>
2021-05-29 15:10:19 +0200

    [sort_my_episodes.sh] Add script

---
+++ etc/sort_my_episodes.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Script for sorting all episodes of the same number into a respective folder
+
+# magic that allows folders with dots to not cause issues
+IFS='
+'
+set -f
+
+# Get range
+echo "I need you to specify the range of episode numbers you want this scripts to search for"
+read -p "Enter minimum range: " range_min
+read -p "Enter maximum range: " range_max
+
+# find and move episodes
+for ((i = range_min ; i <= range_max ; i++)); do
+  printf -v i "%02d" $i # prefix 0 to single digit numbers
+  echo "Searching for episode $i"
+  for j in $(find . -name "*${i}*"); do
+    echo "Moving: ${j}"
+    mkdir -p "Episode ${i}"
+    mv ${j} "./Episode ${i}/"
+  done
+done
