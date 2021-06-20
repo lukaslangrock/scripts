7e9e766174a7ed1a0be1ac14bea21e1876bded67
Lukas Langrock <lukas.langrock@mailbox.org>
2021-06-20 19:56:46 +0200

    [movie_bonus_encoder.sh] Tune parameters

--- ffmpeg/movie_bonus_encoder.sh
+++ ffmpeg/movie_bonus_encoder.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#Designed to quickly encode all bonus content that comes with DVDs and BDs to save space.
+#Designed to encode all bonus content that comes with DVDs and BDs to save space.
 #DO NOT USE THIS FOR THE ACTUAL MOVIE! The scripts only keeps the main audio track and disregards all subs
 #Please check your encode before deleting the original.
 
@@ -11,5 +11,5 @@ set -f
 #actual loop
 for i in $(find . -name '*.mkv'); do
   echo "Encoding mkv: ${i}"
-  ffmpeg -n -i "${i}" -map 0:v:0 -map 0:a:0? -c:v libx264 -crf 18 -preset fast -c:a copy "${i%.*}_[AVC][CRF-18-fast][BD-Encode].mkv"
+  ffmpeg -n -i "${i}" -map 0:v:0 -map 0:a:0? -c:v libx264 -crf 20 -preset slow -c:a copy "${i%.*}_[AVC][CRF-20-slow][BD-Encode].mkv"
 done
