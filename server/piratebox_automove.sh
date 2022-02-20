114aacf96ca36e28ddcbdc4c62675fd5b45288bd
Lukas Langrock <lukas.langrock@mailbox.org>
2022-02-20 15:33:40 +0100

    Update piratebox_automove.sh

--- server/piratebox_automove.sh
+++ server/piratebox_automove.sh
@@ -2,8 +2,21 @@
 # Made to autorun on a PirateBox
 # This script will automatically sort uploaded files
 
-cd /opt/piratebox/share/Shared                                                         
-sudo mv *.png Images/                                                                  
-sudo mv *.jpg Images/                                                                  
-sudo mv *.mp4 Videos/                                                                  
+cd /opt/piratebox/share/Shared
+sudo mv *.png Images/
+sudo mv *.jpg Images/
+sudo mv *.jpeg Images/
+sudo mv *.webp Images/
+sudo mv *.avif Images/
+sudo mv *.jxl Images/
+sudo mv *.mp4 Videos/
+sudo mv *.m4v Videos/
 sudo mv *.mov Videos/
+sudo mv *.mkv Videos/
+sudo mv *.webm Videos/
+sudo mv *.mp3 Music/
+sudo mv *.m4a Music/
+sudo mv *.aac Music/
+sudo mv *.opus Music/
+sudo mv *.flac Music/
+sudo mv *.mka Music/
